8089392a3a16074dc077f95596eb554685969cac
levysah <46534726+levysah@users.noreply.github.com>
2020-02-28 09:25:00 -0600

    VS version 7

--- Version7.0/user27/ValleySportsJS.js
+++ Version7.0/user27/ValleySportsJS.js
@@ -1,11 +1,2 @@
-function Search() {
-    
-     var x = document.getElementById("fname");
-    x.value = x.value.toUpperCase();
-    
-}
 
 
-function ApparelSearch() {
-  prefer = document.forms[0].browsers.value;
-}
