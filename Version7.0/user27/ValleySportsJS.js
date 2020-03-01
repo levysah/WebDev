f978507389202a90b6c40f75035dd276217163ba
levysah <46534726+levysah@users.noreply.github.com>
2020-03-01 16:07:38 -0600

    VS 7

--- Version7.0/user27/ValleySportsJS.js
+++ Version7.0/user27/ValleySportsJS.js
@@ -21,3 +21,4 @@ function Mission () {
 function History () {
     document.getElementById("History").innerHTML = "Valley Sports was established in 1977 on the corner of 1st Avenue and Holmes Street in Historic Downtown Shakopee, MN. In 1978, Dave Jansen began working at Valley Sports and later became the owner in 1990. Dave's nephew, Tony Jansen, spent endless hours at his uncle's shop. Tony finally began collecting a paycheck in 1999, and in 2014, Dave felt Tony was ready to take over the reins of ownership. Since retiring, Dave has continued to be a mentor to Tony. We are proud to have been a staple in Shakopee since 1977. Our store is invested in our community, and has helped kids in the community for over 40 years by sponsoring local sports associations. ";
 }
+
